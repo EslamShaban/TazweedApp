45ff5ecaa7de406a1a4b59fa50f9c6bfcbd9429e
Eslam-comp <eshaban242@gmail.com>
2023-04-12 16:36:33 +0200

    m

--- app/Http/Resources/RequestQuestionResource.php
+++ app/Http/Resources/RequestQuestionResource.php
@@ -14,12 +14,18 @@ class RequestQuestionResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->questionable_type == 'category'){
+            $type = \App\Model\Category::where('id', $this->questionable_id)->first();
+        }else{
+            $type = \App\Model\Service::where('id', $this->questionable_id)->first();
+        }
         return [
             'id'        => $this->id,
             'question'  => $this->question,
             'answer'    => $this->pivot->answer,
             'type'      => $this->questionable_type,
-            'type_id'   => $this->questionable_id
+            'type_id'   => $this->questionable_id,
+            'type_name' => $type->name
         ];
     }
 }
